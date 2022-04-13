c4e505079d6b517b58363b60a7a5045f15dc2382
madan-dinata <madanmr48@gmail.com>
2022-04-13 13:28:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.higherorlower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    int randomNumber;
+    public void randomNumberGenerator() { // for repeating the game
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20) + 1;
+    }
+    public void onGuess(View view) {
+        String message = "";
+        try {
+            EditText editTextNumber = (EditText) findViewById(R.id.editTextNumber);
+            int guessedNumber = Integer.parseInt(editTextNumber.getText().toString());
+            if (guessedNumber < 1 || guessedNumber > 20)
+                message = "the guess must be between the numbers 1 to 20 !!";
+            else if (guessedNumber < randomNumber)
+                message = "Lower !!";
+            else if (guessedNumber > randomNumber)
+                message = "Higher !!";
+            else {
+                message = "You got me !!";
+                randomNumberGenerator(); // kinda recurtion
+            }
+        } catch (Exception e) {
+            message = "field cannot be empty";
+        } finally {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20) + 1; // limit up to 20 from 1
+    }
+}
